90408554d2e7c66674a640dcfa94883780384e9f
nikitagorojanin <ngorojanin@gmail.com>
2019-04-17 00:13:16 +0500

    multiple pipes

---
+++ cmake-build-debug/CMakeFiles/shenn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shenn.dir/main.cpp.o"
+  "shenn.pdb"
+  "shenn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shenn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
