ca77cb53d30389916bc6a7cdf6ebd942dc6a9971
nikitagorojanin <ngorojanin@gmail.com>
2019-05-02 06:23:06 +0500

    Добавлена документация Doxygen

--- shell.h
+++ shell.h
@@ -18,10 +18,16 @@ using Duration = std::chrono::duration<double>;
 using High_Res_Clock = std::chrono::high_resolution_clock;
 using Time_Point = std::chrono::time_point<High_Res_Clock>;
 
+/*!
+ *  \brief Структура, хранящая результат выполнения команды
+ *
+ *  Данную структуру можно использовать если необходимо использовать результат выполнения команды,
+ *  а не выводить его в консоль
+ */
 struct CmdExecResult {
-    string success_result;
-    string error_result;
-    bool success=false;
+    string success_result; ///< Хранит текст результата, в случае успешного выполнения команды
+    string error_result; ///< Хранит текст ошибки, в случае неуспешного выполнения команды
+    bool success=false; ///< Принимает значение true, в случае успешного выполнения команды, иначе false
 
     CmdExecResult()
             : success_result(""), error_result(""), success(false){};
@@ -31,6 +37,12 @@ struct CmdExecResult {
 
 };
 
+/*!
+ *  \brief Основной класс для работы с оболочкой SHELL
+ *
+ *  Данный класс предоставляет набор методов для выполнения команд языка shell и
+ *  работы с результатами их выполнения
+ */
 class Shell {
     vector<string> delimeters = {"||", "|", "&&", ";", "1&>", "2&>", ">>", ">", "(", ")", "&"};
 
@@ -48,14 +60,40 @@ class Shell {
 public:
     Shell() : m_child_time_total(0){};
 
+    /*!
+        Выполняет надор команд разделенных операторами ||, && и ;,
+        а также операторами вывода в файл >, >>, 1&>, 2&>
+        \param[in] command_line Команда пользователя
+    */
     void execute(const string& command_line);
+    /*!
+        Выполняет надор команд разделенных операторами ||, && и ;,
+        а также операторами вывода в файл >, >>, 1&>, 2&>, возвращет результат в переменной result
+        \param[in] command_line Команда пользователя
+        \param[out] result Ссылка на экземпляр структуры CmdExecResult, хранящий результат выполнения команды
+    */
     void execute(const string& command_line, CmdExecResult& result);
 
+    /*!
+        Выполняет надор команд разделенных операторами ||, && и ;,
+        а также операторами вывода в файл >, >>, 1&>, 2&> в бекграунд режиме
+        \param[in] command_line Команда пользователя
+        \param[out] f коллбек-функция, принимающая ссылку на экземпляр стуктуры CmdExecResult,
+        хранящий результат выполнения команды
+    */
     void execute_background(string& command_line, function<void(CmdExecResult&)> f);
 
+    /*!
+        Выполняет надор команд разделенных операторами | (pipe)
+        \param[in] command_line Команда пользователя
+    */
     void execute_piped_command(string& command_line);
+    /*!
+        Выполняет надор команд разделенных операторами | (pipe), возвращет результат в переменной result
+        \param[in] command_line Команда пользователя
+        \param[out] result Ссылка на экземпляр структуры CmdExecResult, хранящий результат выполнения команды
+    */
     void execute_piped_command(string& command_line, CmdExecResult& result);
 };
-
 
 #endif //SHENN_SHELL_H
